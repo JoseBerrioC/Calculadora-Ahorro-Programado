63e9659635b028c3663665fa880c953549c7acd0
Juan Sebastian Lopez Yarce <jlopezyarce@gmail.com>
2026-08-11 19:45:46 -0500

    Cree la funcion calcular totales, la cual calcula salida, total_abonos, total_intereses

--- Logica_calculadora.py
+++ Logica_calculadora.py
@@ -22,3 +22,11 @@ def calcular_cuota(P: float, i: float, n: int) -> float:
         return 0.0
 
 
+def calcular_totales(P: float, i: float, n: int) -> tuple[float, float, float]:
+    """Calcula (salida, total_abonos, total_intereses)."""
+    salida = calcular_cuota(P, i, n)
+    total_abonos = salida * n
+    total_intereses = total_abonos - P
+    return salida, total_abonos, total_intereses
+
+

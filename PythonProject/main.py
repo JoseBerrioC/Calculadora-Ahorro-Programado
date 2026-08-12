b465a0b03959b406ce0d94b6cc1d51bfb955a8e7
José Berrío <josefernandoberrio7@gmail.com>
2026-08-11 22:29:48 -0500

    Update main.py

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -6,13 +6,11 @@ from test_calculadora import (
     TestCasosDeError,
 )
 
-
 def ejecutar_suite(titulo: str, clase_prueba) -> None:
     print(f"=== {titulo} ===\n")
     suite = unittest.TestLoader().loadTestsFromTestCase(clase_prueba)
     unittest.TextTestRunner(verbosity=2).run(suite)
     print("-" * 45 + "\n")
-
 
 if __name__ == "_main_":
     ejecutar_suite("Casos Normales", TestCasosNormales)

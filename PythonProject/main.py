52d112debcf95ba1380cae330862e59a6ecb2819
José Berrío <josefernandoberrio7@gmail.com>
2026-08-12 08:45:32 -0500

    Update main.py

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -1,14 +1,14 @@
 import logica_calculadora
 
 try:
-    print("Este programa le permite calcular la cuota fija a pagar por un prestamo o ahorro programado")
-    monto = float(input("Monto (P): "))
-    tasa = float(input("Tasa de interes por periodo, en % (ej. 3.1): ")) / 100
-    plazo = int(input("Numero de cuotas (n): "))
+    print("Este programa le permite calcular la cuota fija pagada por un prestamo o ahorro programado")
+    monto = float(input("Monto: "))
+    tasa = float(input("Tasa de interes por periodo: ")) / 100
+    plazo = int(input("Numero de cuotas: "))
 
     cuota, total_abonos, total_intereses = logica_calculadora.calcular_totales(monto, tasa, plazo)
 
-    print(f"La cuota a pagar es de:     {round(cuota, 2)}")
+    print(f"La cuota pagada por el banco es de:     {round(cuota, 2)}")
     print(f"Total Abonos:               {round(total_abonos, 2)}")
     print(f"Total Intereses:            {round(total_intereses, 2)}")
 except Exception as err:
